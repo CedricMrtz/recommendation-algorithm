37366ec33c6c6253bffae1314df3b7f155d6502a
rubenyh <rubenyanezhernandez@gmail.com>
2025-11-15 17:23:07 -0800

    Remove build directory from repo

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_front_FILE /home/cedric/Downloads/recommendation-algorithm/build/front)
-set(__QT_DEPLOY_TARGET_front_TYPE EXECUTABLE)
--- build/CMakeFiles/front.dir/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/front.dir/qrc_resources.cpp.o: \
- /home/cedric/Downloads/recommendation-algorithm/build/qrc_resources.cpp \
- /usr/include/stdc-predef.h
